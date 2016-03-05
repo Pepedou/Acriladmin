315dd155b2fbf014e6e1286e009a18251da9c8a5
José Luis Valencia Herrera <pepedou@gmail.com>
2016-03-04 23:32:01 -0600

    Made the employee's roles id its name. Changed verbose names to lowercase so that Django may decide when to capitalize.

--- back_office/models.py
+++ back_office/models.py
@@ -9,18 +9,18 @@ class Address(models.Model):
     A simple geographical address.
     """
 
-    interior_number = models.CharField(verbose_name='No. int.', max_length=10, blank=True)
-    exterior_number = models.CharField(verbose_name='No. ext.', max_length=10)
-    street = models.CharField(verbose_name='Calle', max_length=45)
-    town = models.CharField(verbose_name='Delegación/Municipio', max_length=45, blank=True)
-    city = models.CharField(verbose_name='Ciudad', max_length=45, blank=True)
-    state = models.CharField(verbose_name='Estado', max_length=45, blank=True)
-    country = models.CharField(verbose_name='País', max_length=45, blank=True)
+    interior_number = models.CharField(verbose_name='número interior', max_length=10, blank=True)
+    exterior_number = models.CharField(verbose_name='número exterior', max_length=10)
+    street = models.CharField(verbose_name='calle', max_length=45)
+    town = models.CharField(verbose_name='municipio', max_length=45, blank=True)
+    city = models.CharField(verbose_name='ciudad', max_length=45, blank=True)
+    state = models.CharField(verbose_name='estado', max_length=45, blank=True)
+    country = models.CharField(verbose_name='país', max_length=45, blank=True)
     zip_code = models.CharField(verbose_name='CP', max_length=5, blank=True, validators=[zip_code_regex_validator])
 
     class Meta:
-        verbose_name = 'Dirección'
-        verbose_name_plural = 'Direcciones'
+        verbose_name = 'dirección'
+        verbose_name_plural = 'direcciones'
 
     def __str__(self):
         return "{0}, {1}, {2}".format(self.exterior_number, self.street, self.town)
@@ -39,19 +39,19 @@ class PersonProfile(models.Model):
     def full_name(self):
         return "{0} {1} {2}".format(self.name, self.paternal_last_name, self.maternal_last_name).rstrip()
 
-    name = models.CharField(verbose_name='Nombre(s)', max_length=20)
-    paternal_last_name = models.CharField(verbose_name='Apellido paterno', max_length=20)
-    maternal_last_name = models.CharField(verbose_name='Apellido materno', max_length=20)
-    gender = models.PositiveSmallIntegerField(verbose_name='Género', choices=GENDER_CHOICES)
-    phone = models.CharField(verbose_name='Tel.', max_length=14, blank=True, validators=[phone_regex_validator])
-    email = models.EmailField(verbose_name='Correo electrónico', blank=True,
+    name = models.CharField(verbose_name='nombre(s)', max_length=20)
+    paternal_last_name = models.CharField(verbose_name='apellido paterno', max_length=20)
+    maternal_last_name = models.CharField(verbose_name='apellido materno', max_length=20)
+    gender = models.PositiveSmallIntegerField(verbose_name='género', choices=GENDER_CHOICES)
+    phone = models.CharField(verbose_name='teléfono', max_length=14, blank=True, validators=[phone_regex_validator])
+    email = models.EmailField(verbose_name='correo electrónico', blank=True,
                               validators=[EmailValidator(message='Correo electrónico inválido.')])
-    picture = models.ImageField(verbose_name='Imagen de perfil', blank=True)
-    address = models.ForeignKey(Address, on_delete=models.PROTECT, verbose_name='Dirección', null=True, blank=True)
+    picture = models.ImageField(verbose_name='imagen de perfil', blank=True)
+    address = models.ForeignKey(Address, on_delete=models.PROTECT, verbose_name='dirección', null=True, blank=True)
 
     class Meta:
-        verbose_name = 'Perfil individual'
-        verbose_name = 'Perfiles individuales'
+        verbose_name = 'perfil individual'
+        verbose_name_plural = 'perfiles individuales'
 
     def __str__(self):
         return self.full_name
@@ -61,12 +61,29 @@ class EmployeeRole(models.Model):
     """
     Describes a role assigned to an employee.
     """
-    name = models.CharField(verbose_name='Nombre del rol', max_length=20)
-    description = models.CharField(verbose_name='Descripción del rol', max_length=50)
+    ADMINISTRATOR = 0
+    TELEPHONE_SALES = 1
+    FIELD_SALES = 2
+    INSTALLER = 3
+    DRIVER = 4
+    DOME_PRODUCER = 5
+    WAREHOUSE_CHIEF = 6
+    NAME_CHOICES = (
+        (ADMINISTRATOR, "Administrador"),
+        (TELEPHONE_SALES, "Ventas telefónicas"),
+        (FIELD_SALES, "Ventas en campo"),
+        (INSTALLER, "Instalador"),
+        (DRIVER, "Chofer"),
+        (DOME_PRODUCER, "Productor de domos"),
+        (WAREHOUSE_CHIEF, "Jefe de almacén"),
+    )
+
+    name = models.CharField(verbose_name='nombre del rol', max_length=20, primary_key=True)
+    description = models.CharField(verbose_name='descripción del rol', max_length=50)
 
     class Meta:
-        verbose_name = 'Rol'
-        verbose_name_plural = 'Roles'
+        verbose_name = 'rol'
+        verbose_name_plural = 'roles'
 
     def __str__(self):
         return self.name
@@ -76,16 +93,16 @@ class Employee(models.Model):
     """
     An employee that works for Acrilfrasa.
     """
-    number = models.CharField(verbose_name='Número', max_length=45, primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuario', null=True, blank=True)
-    seniority = models.DateField(verbose_name='Antigüedad')
-    is_active = models.BooleanField(verbose_name='Activo', default=True)
-    role = models.ForeignKey(EmployeeRole, on_delete=models.PROTECT, verbose_name='Rol')
-    profile = models.OneToOneField(PersonProfile, verbose_name='Datos del empleado', on_delete=models.CASCADE)
+    number = models.CharField(verbose_name='número', max_length=45, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='usuario', null=True, blank=True)
+    seniority = models.DateField(verbose_name='antigüedad')
+    is_active = models.BooleanField(verbose_name='activo', default=True)
+    role = models.ForeignKey(EmployeeRole, on_delete=models.PROTECT, verbose_name='rol', limit_choices_to={''})
+    profile = models.OneToOneField(PersonProfile, verbose_name='datos del empleado', on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'Empleado'
-        verbose_name_plural = 'Empleados'
+        verbose_name = 'empleado'
+        verbose_name_plural = 'empleados'
 
     def __str__(self):
         return str(self.profile)
@@ -95,20 +112,20 @@ class OrganizationProfile(models.Model):
     """
     Stores information about an organization or company.
     """
-    name = models.CharField(verbose_name='Name', max_length=45)
-    contact = models.ForeignKey(PersonProfile, on_delete=models.SET_NULL, verbose_name='Persona de contacto', null=True,
+    name = models.CharField(verbose_name='name', max_length=45)
+    contact = models.ForeignKey(PersonProfile, on_delete=models.SET_NULL, verbose_name='persona de contacto', null=True,
                                 blank=True)
-    phone = models.CharField(verbose_name='Tel.', max_length=14, blank=True, validators=[phone_regex_validator])
-    website = models.URLField(verbose_name='Sitio web', max_length=45, blank=True,
+    phone = models.CharField(verbose_name='teléfono', max_length=14, blank=True, validators=[phone_regex_validator])
+    website = models.URLField(verbose_name='sitio web', max_length=45, blank=True,
                               validators=[URLValidator(message="URL inválida.")])
-    email = models.EmailField(verbose_name='Correo electrónico', blank=True,
+    email = models.EmailField(verbose_name='correo electrónico', blank=True,
                               validators=[EmailValidator(message="Correo electrónico inválido.")])
-    picture = models.ImageField(verbose_name='Imagen', blank=True)
-    address = models.ForeignKey(Address, on_delete=models.PROTECT, verbose_name='Dirección', null=True, blank=True)
+    picture = models.ImageField(verbose_name='imagen', blank=True)
+    address = models.ForeignKey(Address, on_delete=models.PROTECT, verbose_name='dirección', null=True, blank=True)
 
     class Meta:
-        verbose_name = 'Perfil de la organización'
-        verbose_name_plural = 'Perfiles de las organizaciones'
+        verbose_name = 'perfil de la organización'
+        verbose_name_plural = 'perfiles de las organizaciones'
 
     def __str__(self):
         return self.name
@@ -118,12 +135,12 @@ class Client(models.Model):
     """
     One of Acrilfrasa's clients.
     """
-    profile = models.OneToOneField(OrganizationProfile, verbose_name='Datos del cliente', on_delete=models.PROTECT)
-    client_since = models.DateField(verbose_name='Antigüedad', auto_now_add=True)
+    profile = models.OneToOneField(OrganizationProfile, verbose_name='datos del cliente', on_delete=models.PROTECT)
+    client_since = models.DateField(verbose_name='antigüedad', auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Cliente'
-        verbose_name_plural = 'Clientes'
+        verbose_name = 'cliente'
+        verbose_name_plural = 'clientes'
 
     def __str__(self):
         return str(self.profile)
@@ -133,14 +150,14 @@ class BranchOffice(models.Model):
     """
     A location involved in the business activities of the firm.
     """
-    profile = models.OneToOneField(OrganizationProfile, verbose_name='Datos de la sucursal', on_delete=models.CASCADE)
-    administrator = models.ForeignKey(Employee, on_delete=models.PROTECT, verbose_name='Administrador de la sucursal',
+    profile = models.OneToOneField(OrganizationProfile, verbose_name='datos de la sucursal', on_delete=models.CASCADE)
+    administrator = models.ForeignKey(Employee, on_delete=models.PROTECT, verbose_name='administrador de la sucursal',
                                       related_name="administrated_branches")
-    employees = models.ManyToManyField(Employee, verbose_name='Empleados de la sucursal', blank=True)
+    employees = models.ManyToManyField(Employee, verbose_name='empleados de la sucursal', blank=True)
 
     class Meta:
-        verbose_name = 'Sucursal'
-        verbose_name_plural = 'Sucursales'
+        verbose_name = 'sucursal'
+        verbose_name_plural = 'sucursales'
 
     def __str__(self):
         return str(self.profile)
