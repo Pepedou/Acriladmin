dd24e8a617143f2dc501340ca0334809a15394ac
José Luis Valencia Herrera <pepedou@gmail.com>
2016-03-13 16:28:31 -0600

    Added new unit test for Invoice.

--- finances/tests/test_models.py
+++ finances/tests/test_models.py
@@ -23,3 +23,18 @@ class InvoiceTestCase(TestCase):
         ])
 
         self.assertTrue(invoice.has_been_paid(), "Method should state invoice has been paid.")
+
+    def test_invoice_has_not_been_paid(self):
+        """
+        Tests if the method 'has_been_paid' returns False when an invoice's
+        amount hasn't been covered by one or more transactions.
+        """
+        invoice = Invoice.objects.create(total=100.00)
+        invoice.save()
+
+        Transaction.objects.bulk_create([
+            Transaction(invoice=invoice, amount=10.00),
+            Transaction(invoice=invoice, amount=20.00),
+        ])
+
+        self.assertFalse(invoice.has_been_paid(), "Method should state invoice has not been paid.")
