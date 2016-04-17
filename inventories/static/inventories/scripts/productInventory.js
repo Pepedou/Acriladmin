3000a9c091c15f1911e93965ce8bdb4aa323d1b1
José Luis Valencia Herrera <pepedou@gmail.com>
2016-04-16 20:44:14 -0500

    Added JQuery DataTables.

---
+++ inventories/static/inventories/scripts/productInventory.js
@@ -0,0 +1,12 @@
+/**
+ * Created by José Luis Valencia Herrera on 16/04/16.
+ */
+
+$(document).ready(function () {
+    //noinspection JSUnresolvedFunction
+    $("#inventoryTable").DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json"
+        }
+    });
+});
