107cb0b5a6d4ab82d120be54d46b57413b408ad1
José Luis Valencia Herrera <pepedou@gmail.com>
2016-05-24 22:00:27 -0500

    Added documentation and a new function to get the price by product description.

--- finances/static/finances/scripts/productPrice.js
+++ finances/static/finances/scripts/productPrice.js
@@ -12,6 +12,11 @@ function ProductPrice() {
     self.authorized_by_id = 0;
     self.successCallback = null;
 
+    /**
+     * Obtains the product's price for the product that matches the given ID.
+     * @param productId The ID of the product.
+     * @param successCallback The callback to be run on success.
+     */
     this.getProductPriceByProductId = function (productId, successCallback) {
         var url = APP_DOMAIN + "/api/finances/productprice/" + productId;
         var ajaxParams = {
@@ -24,8 +29,34 @@ function ProductPrice() {
         $.ajax(url, ajaxParams);
     };
 
-    this.onAjaxSuccess = function (data, textStatus) {
-        if (textStatus !== "success") {
+    /**
+     * Obtains the product's price for the product that matches the given description.
+     * @param productDescription The exact description of the product ID.
+     * @param successCallback The callback to be run on success.
+     */
+    this.getProductPriceByProductDescription = function (productDescription, successCallback) {
+        var url = APP_DOMAIN + "/api/finances/productprice/";
+        var ajaxParams = {
+            method: 'GET',
+            data: {
+                product_description: encodeURIComponent(productDescription)
+            },
+            success: this.onAjaxSuccess,
+            error: this.onAjaxError
+        };
+
+        this.successCallback = successCallback;
+
+        $.ajax(url, ajaxParams);
+    };
+
+    /**
+     * The success callback for the AJAX call.
+     * @param data Returned data.
+     * @param textStatus The returned status.
+     */
+    this.onAjaxSuccess = function (data, textStatus, jqXHR) {
+        if (textStatus !== "success" || jqXHR.status !== 200) {
             alert("No se pudo obtener el precio individual de los productos.");
             return;
         }
@@ -36,10 +67,14 @@ function ProductPrice() {
         self.authorized_by_id = data.authorized_by;
 
         if (self.successCallback) {
-            self.successCallback(data);
+            self.successCallback(data, textStatus, jqXHR);
         }
     };
 
+    /**
+     * The error callback for the AJAX call.
+     * @param jqXHR Contains the error status.
+     */
     this.onAjaxError = function (jqXHR) {
         var errorMessage;
 
@@ -47,7 +82,7 @@ function ProductPrice() {
             errorMessage = "El precio del producto seleccionado no se ha dado de alta.";
         }
         else {
-            alert("No se pudo actualizar el costo del producto.");
+            alert("No se pudo obtener el precio del producto.");
         }
 
         alert(errorMessage);
