5af141e93de34fec9fd460ab309863a4c411e480
José Luis Valencia Herrera <pepedou@gmail.com>
2016-06-08 23:16:45 -0500

    Uploaded Excel files are now converted to ProductInventoryItems and saved to the related inventory.

--- inventories/forms/productsinventory_forms.py
+++ inventories/forms/productsinventory_forms.py
@@ -1,8 +1,10 @@
 import pyexcel
 from django import forms
 from django.core.exceptions import ValidationError
+from django.db.models import Q
 from django.forms import ModelForm
-from inventories.models import ProductsInventory
+from django.forms.utils import ErrorList
+from inventories.models import ProductsInventory, ProductInventoryItem, Product
 from inventories.validators import validate_file_extension
 
 
@@ -17,26 +19,25 @@ class AddOrChangeProductsInventoryForm(ModelForm):
         model = ProductsInventory
         fields = '__all__'
 
+    def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None,
+                 initial=None, error_class=ErrorList, label_suffix=None,
+                 empty_permitted=False, instance=None):
+        super(AddOrChangeProductsInventoryForm, self).__init__(data, files, auto_id, prefix, initial, error_class,
+                                                               label_suffix, empty_permitted, instance)
+        self.excel_data_dict = {}
+
     def clean_excel_file(self):
         """
         Validates that the uploaded file has a valid extension and is formatted
         properly.
         """
-        super(AddOrChangeProductsInventoryForm, self).clean_excel_file()
-        valid_extensions = ['.xls', '.xlsx']
 
         file = self.cleaned_data.get('excel_file')
 
         if file is None:
             return
 
-        validate_file_extension(file, valid_extensions)
-
-        file_extension = file.name.split(".")[1]
-
-        sheet = pyexcel.load_from_memory(file_extension, file.read(), name_columns_by_row=0,
-                                         name_rows_by_column=-1)
-        excel_data = pyexcel.to_dict(sheet)
+        excel_data = AddOrChangeProductsInventoryForm._excel_file_to_dict(file)
 
         if 'SKU' not in excel_data:
             raise ValidationError(
@@ -70,3 +71,56 @@ class AddOrChangeProductsInventoryForm(ModelForm):
                 raise ValidationError(
                     'La columna de "Cantidad" sólo puede contener números enteros. {0} no es un valor válido'.format(
                         item))
+
+        self.excel_data_dict = excel_data
+
+    @staticmethod
+    def _excel_file_to_dict(file):
+        """
+        Loads an Excel file and transforms it into a dictionary. It uses the first row's cells
+        as keys for the dictionary and the columns are the contents.
+        :param file: The Excel file to transform.
+        :return: A dictionary of arrays.
+        """
+        valid_extensions = ['.xls', '.xlsx']
+
+        validate_file_extension(file, valid_extensions)
+
+        file_extension = file.name.split(".")[1]
+        sheet = pyexcel.load_from_memory(file_extension, file.read(), name_columns_by_row=0, name_rows_by_column=-1)
+
+        return pyexcel.to_dict(sheet)
+
+    def save(self, commit=True):
+        """
+        Overrides the save method in order to add the product inventory items listed in
+        the uploaded Excel file, if one was uploaded.
+        """
+        inventory = super(AddOrChangeProductsInventoryForm, self).save(commit)
+
+        if len(self.excel_data_dict) == 0:
+            return inventory
+
+        non_existing_products = []
+
+        for i, product_sku in enumerate(self.excel_data_dict['SKU']):
+            quantity = self.excel_data_dict['Cantidad'][i]
+            new_item = ProductInventoryItem.objects.filter(Q(product__sku=product_sku) &
+                                                           Q(inventory=inventory)).first()
+
+            if new_item is None:
+                product = Product.objects.filter(sku=product_sku).first()
+
+                if product is None:
+                    # TODO: Raise warning in view
+                    non_existing_products.append(product_sku)
+                    continue
+
+                new_item = ProductInventoryItem()
+                new_item.product = product
+                new_item.inventory = inventory
+
+            new_item.quantity += quantity
+            new_item.save()
+
+        return inventory
